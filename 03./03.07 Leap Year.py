68b20d3f23c14a8daaa8c373463da148df0887a5
dcanfield-ualr <156949237+dcanfield-ualr@users.noreply.github.com>
2024-02-05 05:07:29 +0000

    bedtime

--- 03./03.07 Leap Year.py
+++ 03./03.07 Leap Year.py
@@ -1,7 +1,15 @@
-#Prompt for a 4 digit year'
-#If the year is a leap year, print "LEAP YEAR"
-#otherwise print "COMMON YEAR".
+#Prompt for a 4 digit year
+#If the year is a leap year, print "LEAP YEAR", otherwise print "COMMON YEAR".
 #The rules in Gregorian calendar are as follows:
-#A year is a leap year if its number is exactly divisible by 4 
-#is not exactly divisible by 100
-#A year is always a leap year if its number is exactly divisible by 400
+#a leap year is exactly divisible by 4 is not exactly divisible by 100
+#a leap year is exactly divisible by 400
+
+year = int(input("Enter Year: "))
+
+#if the remainder of year/4 = 0 and remainder of year/100 doesnt = 0 
+#if the remainder of year/400 = 0
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("LEAP YEAR")
+else:
+    print("COMMON YEAR")
+
---
+++ 04./04.Project.py
@@ -0,0 +1,3 @@
+#Create a program that displays a list or prime number in a specified range.
+#A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+#Hint: For a given value N, Loop from 2 to (half of N) + 1 and check to see if any number evenly divides into N.
