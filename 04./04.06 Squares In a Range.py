2eb06c1cba70bf084b382c1ea7b9cfbe9bd793a4
dcanfield-ualr <156949237+dcanfield-ualr@users.noreply.github.com>
2024-02-07 16:45:57 +0000

    commit

--- 04./04.06 Squares In a Range.py
+++ 04./04.06 Squares In a Range.py
@@ -2,3 +2,15 @@
 #Be sure to print the formula as shown.
 #There are no spaces around * and =.
 #You can use either the sep= argument of the print() statement or the .format method.
+
+# Read the integer N
+N = int(input("Enter N: "))
+num = 1
+sum = 0
+
+for i in range(1, N+1): 
+    num *= i
+    print(num)
+    sum += num
+    print(sum)
+print("Sum of Factorials: ",sum)
