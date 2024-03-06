9037c6882667fff3d2c44c4211e91a9323c6608b
dcanfield-ualr <156949237+dcanfield-ualr@users.noreply.github.com>
2024-03-06 14:04:28 +0000

    Project 7

--- 07./07.Project/07.Project.py
+++ 07./07.Project/07.Project.py
@@ -3,39 +3,36 @@ input_file = open("07./07.Project/07.Project Angles Input.txt", "r")
 output_file = open("07./07.Project/07.Project Angles Output.txt", "w")
 #set initial variables
 records = 0
-
-def ParseDegreeString(string):
-    degrees = string[1:2]
-    minutes = string[3:4]
-    seconds = string[5:6]
+"""
+def ParseDegreeString(line):
+    degrees = line[1:2]
+    minutes = line[3:4]
+    seconds = line[5:6]
     return (degrees, minutes, seconds)
 
 def DDMMStoDecimal(degrees,minutes,seconds):
-    decimal_degrees = degrees + (minutes/60) + (seconds/3600)
+    decimal_degrees = (degrees) + (minutes/60) + (seconds/3600)
     return (decimal_degrees)
 
 
 for line in input_file:
     ParseDegreeString(line)
-    degrees = ParseDegreeString(degrees)
+    degrees = int(ParseDegreeString(degrees))
     minutes = ParseDegreeString(minutes)
     seconds = ParseDegreeString(seconds)
     decimal_degrees = DDMMStoDecimal(degrees, minutes, seconds)
+    output_file.write(DDMMStoDecimal(decimal_degrees))
+    records += 1
+"""
+
+
+for line in input_file:
+    degrees = int(line[1:2])
+    minutes = int(line[3:4])
+    seconds = int(line[5:6])
+    decimal_degrees = (degrees) + (minutes/60) + (seconds/3600)
     output_file.write(decimal_degrees)
     records += 1
-
-"""def circumference(radius):
-    return 2 * math.pi * radius
-
-def area(radius):
-    return math.pi * radius ** 2
-
-# Open and read the Radius.txt file
-file = open("06./06.01 Radius.txt")
-for num in file:
-    radius = float(num)
-    print("{:>15.5f} {:>15.5f} {:>15.5f} {:>15.5f}".format(radius, diameter(radius), circumference(radius), area(radius)))"""
-
 
 # Close both files
 input_file.close()
