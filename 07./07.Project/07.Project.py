4861b49fdf48d80695b5ade5621a11852b913239
dcanfield-ualr <156949237+dcanfield-ualr@users.noreply.github.com>
2024-03-06 15:46:47 +0000

    07.project working

--- 07./07.Project/07.Project.py
+++ 07./07.Project/07.Project.py
@@ -3,42 +3,37 @@ input_file = open("07./07.Project/07.Project Angles Input.txt", "r")
 output_file = open("07./07.Project/07.Project Angles Output.txt", "w")
 #set initial variables
 records = 0
-"""
-def ParseDegreeString(line):
-    degrees = line[1:2]
-    minutes = line[3:4]
-    seconds = line[5:6]
-    return (degrees, minutes, seconds)
 
-def DDMMStoDecimal(degrees,minutes,seconds):
-    decimal_degrees = (degrees) + (minutes/60) + (seconds/3600)
-    return (decimal_degrees)
+def parseDegreeString(ddmmss):
+    degree_idx = ddmmss.find(chr(176))
+    minute_idx = ddmmss.find("'")
+    second_idx = ddmmss.find('"')
 
+    degrees = float(ddmmss[:degree_idx])
+    minutes = float(ddmmss[degree_idx + 1:minute_idx])
+    seconds = float(ddmmss[minute_idx + 1:second_idx])
 
+    return degrees, minutes, seconds
+
+def DDMMSStoDecimal(degrees, minutes, seconds):
+    decimal_degrees = degrees + (minutes/60) + (seconds/3600)
+    return decimal_degrees
+
+# Read angles from input file
+#with open("input.txt", "r") as file:
+#    angles = file.readlines()
+
+# Convert angles to decimal degrees and write to output file
+#with open("output.txt", "w") as file:
+    
 for line in input_file:
-    ParseDegreeString(line)
-    degrees = int(ParseDegreeString(degrees))
-    minutes = ParseDegreeString(minutes)
-    seconds = ParseDegreeString(seconds)
-    decimal_degrees = DDMMStoDecimal(degrees, minutes, seconds)
-    output_file.write(DDMMStoDecimal(decimal_degrees))
-    records += 1
-"""
-
-
-for line in input_file:
-    degrees = int(line[1:2])
-    minutes = int(line[3:4])
-    seconds = int(line[5:6])
-    decimal_degrees = (degrees) + (minutes/60) + (seconds/3600)
-    output_file.write(decimal_degrees)
+    degrees, minutes, seconds = parseDegreeString(line.strip())
+    decimal_degrees = DDMMSStoDecimal(degrees, minutes, seconds)
+    output_file.write(str(decimal_degrees))
+    output_file.write("\n")
     records += 1
 
 # Close both files
 input_file.close()
 output_file.close()
 print(records, "records processed")
-
-
-
-
